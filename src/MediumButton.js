6e01297b82a7244c6aa93445361568a561cda233
M. Kölling <assembla@periscope.de>
2016-11-04 17:02:21 +0100

    Get parent node - issue #13 #6
    issue #13 #6

--- src/MediumButton.js
+++ src/MediumButton.js
@@ -1,23 +1,23 @@
 /**
  *   MediumButton  1.0 (24.02.2015)
  *   MIT (c) Patrick Stillhart
- *  
+ *
  */
 function MediumButton(options) {
-    if (options.label === undefined || !/\S{1}/.test(options.label) || 
-		options.start === undefined || !/\S{1}/.test(options.start) || 
-		options.end === undefined || !/\S{1}/.test(options.end)) {
-		
-		if(options.label === undefined || !/\S{1}/.test(options.label) ||
-		   options.action === undefined || !/\S{1}/.test(options.action)) {
-			console.error('[Custom-Button] You need to specify "label", "start" and "end" OR "label" and "action"');	
-			return;
-		} 
+    if (options.label === undefined || !/\S{1}/.test(options.label) ||
+        options.start === undefined || !/\S{1}/.test(options.start) ||
+        options.end === undefined || !/\S{1}/.test(options.end)) {
+
+        if(options.label === undefined || !/\S{1}/.test(options.label) ||
+            options.action === undefined || !/\S{1}/.test(options.action)) {
+            console.error('[Custom-Button] You need to specify "label", "start" and "end" OR "label" and "action"');
+            return;
+        }
     }
-	
-	options.start = (options.start === undefined) ? '' : options.start;
-	options.end = (options.end === undefined) ? '' : options.end;
-	
+
+    options.start = (options.start === undefined) ? '' : options.start;
+    options.end = (options.end === undefined) ? '' : options.end;
+
     this.options = options;
     this.button = document.createElement('button');
     this.button.className = 'medium-editor-action';
@@ -25,26 +25,26 @@ function MediumButton(options) {
     this.button.onclick = function() {
         // Get Current Value
         var html = getCurrentSelection(), sel = window.getSelection();
-		
-        //Modify Content 
-		var mark = true;
+
+        var parent = sel.anchorNode.parentElement;
+
+        //Modify Content
+        var mark = true;
         if (options.start === undefined || html.indexOf(options.start) == -1 && html.indexOf(options.end) == -1) {
-		
-			if(options.action != undefined) html = options.action(html, true);
-		
+
+            if(options.action != undefined) html = options.action(html, true, parent);
+
             html = options.start + html + options.end;
 
         } else { //clean old
-			if(options.action != undefined) html = options.action(html, false);
-			html = String(html).split(options.start).join('');
-			html = String(html).split(options.end).join('');
+            if(options.action != undefined) html = options.action(html, false, parent);
+            html = String(html).split(options.start).join('');
+            html = String(html).split(options.end).join('');
         }
-		
-		
-		
-		
-		var range;
-    var fragment;
+
+
+        var range;
+        var fragment;
         //Set new Content
         if (sel.getRangeAt && sel.rangeCount) {
             range = window.getSelection().getRangeAt(0);
@@ -62,7 +62,7 @@ function MediumButton(options) {
                 while ((child = div.firstChild)) {
                     fragment.appendChild(child);
                 }
-				
+
             }
             var firstInsertedNode = fragment.firstChild;
             var lastInsertedNode = fragment.lastChild;
@@ -74,7 +74,7 @@ function MediumButton(options) {
             sel.removeAllRanges();
             sel.addRange(range);
         }
-	
+
     };
 
 }
@@ -84,7 +84,7 @@ MediumButton.prototype.getButton = function() {
 };
 
 MediumButton.prototype.checkState = function(node) {
-	var html = getCurrentSelection();
+    var html = getCurrentSelection();
     if (this.options.start != '' && html.indexOf(this.options.start) > -1 && html.indexOf(this.options.end) > -1) {
         this.button.classList.add('medium-editor-button-active');
     }
@@ -93,22 +93,22 @@ MediumButton.prototype.checkState = function(node) {
 
 function getCurrentSelection() {
 
-	var html = '', sel;
-       if (typeof window.getSelection != 'undefined') {
-           sel = window.getSelection();
-           if (sel.rangeCount) {
-               var container = document.createElement('div');
-               for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                   container.appendChild(sel.getRangeAt(i).cloneContents());
-               }
-               html = container.innerHTML;
-           }
-       } else if (typeof document.selection != 'undefined') {
-           if (document.selection.type == 'Text') {
-               html = document.selection.createRange().htmlText;
-           }
-       }
-	
-	return html;
-	
+    var html = '', sel;
+    if (typeof window.getSelection != 'undefined') {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            var container = document.createElement('div');
+            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                container.appendChild(sel.getRangeAt(i).cloneContents());
+            }
+            html = container.innerHTML;
+        }
+    } else if (typeof document.selection != 'undefined') {
+        if (document.selection.type == 'Text') {
+            html = document.selection.createRange().htmlText;
+        }
+    }
+
+    return html;
+
 }
